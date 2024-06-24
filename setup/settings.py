b4835c1bd0e37d498bc7e638291f6ced21d7a2a1
Markos Alves pereira <101573252+Markosalves12@users.noreply.github.com>
2024-06-24 16:22:25 -0300

    configuração banco de dados

--- setup/settings.py
+++ setup/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 import django_heroku
 from dotenv import load_dotenv
+import dj_database_url
 
 
 load_dotenv()
@@ -94,10 +95,7 @@ WSGI_APPLICATION = "setup.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 # Password validation
